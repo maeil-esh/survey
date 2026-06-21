65ddc59b3fdf15059239bc771883bb0b72178b66
maeil-esh <hiholme1800@gmail.com>
2026-06-21 23:31:29 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
  * 2) 배포된 /exec URL을 아래 GAS_API_URL에 붙여넣기
  */
 window.SAFE_TALK_CONFIG = {
-  GAS_API_URL: 'PASTE_GAS_WEB_APP_EXEC_URL_HERE',
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbz1SDH5f5doD0DfXYLqLESm3ZJpAIJfnLT3WQFXfIgaoA9IU9LVdppT_TuaUR9-xYfBMA/exec',
   REFRESH_MS_VOTE: 1000,
   REFRESH_MS_DISPLAY: 1000,
   REFRESH_MS_ADMIN: 2000
